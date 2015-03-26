0c95f814a0e88b862e22194511f08ff7e1db7d4a
Martin Drapeau <martindrapeau@gmail.com>
2015-03-25 20:30:39 -0400

    .

--- js/src/main.js
+++ js/src/main.js
@@ -73,7 +73,7 @@ $(window).on("load", function() {
       // Our world
       // Reserve bottom of canvas for input and editor
       this.world = new Backbone.World(
-        _.extend({viewportBottom: 156}, window._world), {
+        _.extend({viewportBottom: 156}, window._world, {state: "pause"}), {
         input: this.input,
         camera: this.camera
       });
@@ -146,7 +146,9 @@ $(window).on("load", function() {
 
       // If we have Application Cache active, load the latest world
       this.loadWorld();
-      this.world.drawBackground = true;
+
+      // Hack to get background to draw properly in CocoonJS
+      setTimeout(this.toggleState, 1000);
     },
     toggleState: function(e) {
       var state = this.world.get("state");
